fe4e0f6fa20422caafa86494078ab0ebbef14a3c
John Dewey <john@dewey.ws>
2012-10-04 15:49:34 -0700

    Added librarian for chefspec jenkins jobs

--- spec/megaraid_spec.rb
+++ spec/megaraid_spec.rb
@@ -1,23 +1,26 @@
 require "chefspec"
 
 describe "raid::megaraid" do
+  before do
+    @megacli = "/usr/local/bin/MegaCli64"
+    @adapter = "0"
+    Chef::Recipe.any_instance.stub(:include_recipe)
+  end
   let(:chef_run) { ChefSpec::ChefRunner.new.converge "raid::megaraid" }
-  let(:megacli) { "/usr/local/bin/MegaCli64" }
-  let(:adapter) { "0" }
 
   describe "array" do
     it "clears" do
-      chef_run.should execute_command "#{megacli} -cfglddel -lall -a#{adapter}; sleep 30"
+      chef_run.should execute_command "#{@megacli} -cfglddel -lall -a#{@adapter}; sleep 30"
     end
 
     it "creates" do
-      command = %Q{#{megacli} -CfgSpanAdd -r10 -Array0[["245:0", "245:1", "245:2", "245:3"]] -Array1[["245:4", "245:5", "245:6", "245:7"]] WB NORA -strpsz64 -a#{adapter}}
+      command = %Q{#{@megacli} -CfgSpanAdd -r10 -Array0[["245:0", "245:1", "245:2", "245:3"]] -Array1[["245:4", "245:5", "245:6", "245:7"]] WB NORA -strpsz64 -a#{@adapter}}
 
       chef_run.should execute_command command
     end
 
     it "adds spares" do
-      command = %Q{#{megacli} -pdhsp -set -physdrv [["245:8", "245:9", "245:10", "245:11"]] -a#{adapter}}
+      command = %Q{#{@megacli} -pdhsp -set -physdrv [["245:8", "245:9", "245:10", "245:11"]] -a#{@adapter}}
 
       chef_run.should execute_command command
     end
